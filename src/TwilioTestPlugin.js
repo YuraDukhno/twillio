3d486bdea0f38bb7112f65014efb0f24d3682266
YuraD <yurad@net-bet.net>
2024-06-26 22:18:49 +0300

    working plugin add

--- src/TwilioTestPlugin.js
+++ src/TwilioTestPlugin.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { FlexPlugin } from '@twilio/flex-plugin';
-
 import CustomTaskList from './components/CustomTaskList/CustomTaskList';
 
 const PLUGIN_NAME = 'TwilioTestPlugin';
@@ -15,8 +14,39 @@ export default class TwilioTestPlugin extends FlexPlugin {
    * Use this to modify any UI components or attach to the actions framework
    *
    * @param flex { typeof import('@twilio/flex-ui') }
+   * @param manager { Flex.Manager }
    */
   async init(flex, manager) {
+    // Automatically accept and select tasks when a reservation is created
+    manager.workerClient.on('reservationCreated', reservation => {
+      flex.Actions.invokeAction('AcceptTask', { sid: reservation.sid });
+      flex.Actions.invokeAction('SelectTask', { sid: reservation.sid });
+    });
+
+    // Listen for task completion events
+    manager.events.addListener('taskCompleted', ({task}) => {
+      const { from, to } = task['_task']?.attributes;
+
+      const body = {
+        to: from,
+        from: to,
+        body: 'Thank you for calling us',
+      };
+
+      const options = {
+        method: 'POST',
+        body: new URLSearchParams(body),
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        },
+      };
+    
+      fetch('https://yuras-first-7376.twil.io/send_sms', options)
+        .then((res) => res)
+        .catch((err) => console.error(err));
+    });
+
+    // Add CustomTaskList component to the UI
     const options = { sortOrder: -1 };
     flex.AgentDesktopView.Panel1.Content.add(<CustomTaskList key="TwilioTestPlugin-component" />, options);
   }
--- src/components/CustomTaskList/CustomTaskList.jsx
+++ src/components/CustomTaskList/CustomTaskList.jsx
@@ -1,24 +1,45 @@
 import React, { useState } from 'react';
-
 import { Alert } from '@twilio-paste/core/alert';
 import { Theme } from '@twilio-paste/core/theme';
 import { Text } from '@twilio-paste/core/text';
+import { Box } from '@twilio-paste/core/box';
+import { Card } from '@twilio-paste/core/card';
+import { Button } from '@twilio-paste/core/button';
+import { Stack } from '@twilio-paste/core/stack';
+import { CloseIcon } from '@twilio-paste/icons/esm/CloseIcon';
 
-const CustomTaskList = () => {
-  const [isOpen, setIsOpen] = useState(true);
-  if (!isOpen) {
-    return null;
-  }
+const CustomTaskList = ({ tasks }) => {
+  const [dismissedTasks, setDismissedTasks] = useState(new Set());
 
-  const dismiss = () => setIsOpen(false);
+  // Check if there are any tasks that are not dismissed
+  const taskArray = Array.from(tasks.values());
+  const hasTasks = taskArray.some(task => !dismissedTasks.has(task.sid));
+
+  // Dismiss the task by adding its SID to the dismissedTasks set
+  const dismiss = (taskSid) => {
+    setDismissedTasks(new Set(dismissedTasks).add(taskSid));
+  };
 
   return (
     <Theme.Provider theme="default">
-      <Alert onDismiss={dismiss} variant="neutral">
-        <Text>
-          This is a dismissible demo component.
-        </Text>
-      </Alert>
+      <Box>
+        {hasTasks && (
+          <Stack orientation="vertical" spacing="space60">
+            {taskArray.map(task => (
+              !dismissedTasks.has(task.sid) && (
+                <Card key={task.sid} padding="space70" boxShadow="shadowCard">
+                  <Box display="flex" justifyContent="space-between" alignItems="center">
+                    <Text color="colorTextNeutral" fontSize="1.5rem" as="h3">You have an active task.</Text>
+                    <Button variant="destructive_secondary" onClick={() => dismiss(task.sid)}>
+                      <CloseIcon decorative={false} title="Dismiss task" />
+                    </Button>
+                  </Box>
+                </Card>
+              )
+            ))}
+          </Stack>
+        )}
+      </Box>
     </Theme.Provider>
   );
 };
